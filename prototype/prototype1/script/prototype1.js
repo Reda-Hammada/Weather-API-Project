9588a2e402a66f9ed7dea5538ae40e8398fc4765
reda hammada <hammada.reda.dev@gmail.com>
2022-01-12 18:49:27 +0100

    update

--- prototype/prototype1/script/prototype1.js
+++ prototype/prototype1/script/prototype1.js
@@ -1,5 +1,5 @@
 let city = document.getElementById('city');
-let url ='api.openweathermap.org/data/2.5/weather?q=frankfurt&appid=c259124f52b8b6cc2e6fdd0caa3f7f4b';
+let url ='';
 
 fetch(url)
 .then(response => response.json())
