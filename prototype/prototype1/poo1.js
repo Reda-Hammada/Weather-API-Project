7a486cc3df2c9d4dd9e3527d61e59580c312dc9e
redahm <hammada.reda.dev@gmail.com>
2022-01-14 09:50:33 +0100

    oop added

---
+++ prototype/prototype1/poo1.js
@@ -0,0 +1,23 @@
+class town {
+
+    #_temp;
+    #_city;
+
+
+    get temp(){
+        return this.#_temp;
+    }
+
+    set temp(value){
+
+        this.#_temp = value;
+    }
+
+    get city(){
+        return this.#_city;
+    }
+
+    set city(value){
+        this.#_city = value;
+    }
+}
