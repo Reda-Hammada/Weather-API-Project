36519663b45e294734fb8d92dc88e335dbe65752
reda hammada <hammada.reda.dev@gmail.com>
2022-01-15 23:15:05 +0100

    update

--- prototype/prototype0/ouverage.js
+++ prototype/prototype0/ouverage.js
@@ -2,15 +2,14 @@ var _work = new workManager();
 
 document.getElementById('formSubmit').addEventListener("submit", function(event){
     event.preventDefault();
-    var work  = readWork();
+    var Work  = readWork();
     insertNewRow();
     resetForm();
    
 })
 
 
-function readWork() {
-
+    function readWork() { 
     var work = new work();
     work.title = document.getElementById("inputTitle").value;
     return work;
@@ -33,6 +32,23 @@ function insertNewRow(){
     for(var i = 0; i < workList.length; i++){
         var newRow = tableBody.insertRow(table.length);
         newRow.insertCell(0).innerHTML = workList[i].id;
+        cell2 = newRow.insertCell(1);
+        cell2.innerHTML = workList[i].title;
+
+        var editButton = document.createElement("button");
+        editButton.document.createTextNode("edit");
+        editButton.setAttribute("onclick", 'onEdit(this)')
+        var deleteButton  = document.createElement("button");
+        deleteButton.document.createTextNode("Delete");
+        deleteButton.setAttribute("onclick", "onDelete(this)");
+
+        cell3 = newRow.insertCell(2);
+        cell3.appendChild(editButton)
+        cell3.appendChild(deleteButton)
+
+
+
+
     }
 
 }
