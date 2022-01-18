d37a158a30486748c1fbb9cb25fb323aed487499
redahm <hammada.reda.dev@gmail.com>
2022-01-18 11:58:22 +0100

    update

--- prototype/prototype2/prototype2.js
+++ prototype/prototype2/prototype2.js
@@ -4,12 +4,14 @@ let url;
 let paragraph ;
 var donnes;
 
-// input
-city = document.getElementById('inputBar');
-paragraph = document.getElementById('paragraph');
-url = "https://api.openweathermap.org/data/2.5/weather?q=&appid=0bf1faee1a5ca7e5e899dd845e4e0d44";
 
-fetch(url)
+
+function show(){
+  city = document.getElementById('inputBar').value;
+  paragraph = document.getElementById('paragraph');
+  url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=0bf1faee1a5ca7e5e899dd845e4e0d44';
+
+  fetch(url)
 .then(function(response){
 
     return response.json();
@@ -19,13 +21,10 @@ fetch(url)
 .then(function(data){
 
   donnes = data;
- 
+  paragraph.innerHTML = donnes.main.temp;
+
 })
 
-
-function show(){
-    
-  paragraph.innerHTML = donnes.main.temp;
 
 }
 
