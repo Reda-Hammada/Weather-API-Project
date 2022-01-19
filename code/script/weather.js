398901d22c2e27588153225543adc7c1288eb500
redahm <hammada.reda.dev@gmail.com>
2022-01-19 16:55:31 +0100

    update

--- code/script/weather.js
+++ code/script/weather.js
@@ -7,7 +7,9 @@ let form;
 // treatment 
 document.getElementById('searchForm').addEventListener("submit", function show(event){
     event.preventDefault();
-  
+    if(document.getElementById('searchForm').click){
+        document.querySelector('.info-container').sytle.display = 'bloc';
+    }
     // input 
     search = document.getElementById('searchBar').value;
     url = "https://api.openweathermap.org/data/2.5/weather?q="+search+"&appid=0bf1faee1a5ca7e5e899dd845e4e0d44&units=metric";
@@ -26,13 +28,14 @@ document.getElementById('searchForm').addEventListener("submit", function show(e
            city = new City();
            city.name = data.name;
            city.country = data.sys.country;
-           city.description = data.weather.description;
+           city.description = data.weather[0].description;
            city.humidity = data.main.temp;
            city.wind = data.wind.speed;
         
-           list1 = document.getElementById('list1');
-           list1.innerHTML = "location: " + city.name + ',' + city.country;
-           list2 = document.getElementById('list2').innerHTML = "weather: " + city.description;
+           info1 = document.getElementById('info1');
+           info1.innerHTML = "location: " + city.name + ',' + city.country;
+           info2 = document.getElementById('info2').innerHTML =  city.description;
+           info3 = document.getElementById('info3').innerHTML =  city.humidity + "°C";
     })
 
     
