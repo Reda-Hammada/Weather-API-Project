f0c1a2556eeaaaef725409057cece52d6f97aa30
redahm <hammada.reda.dev@gmail.com>
2022-01-19 12:14:34 +0100

    API tested

---
+++ code/script/city.js
@@ -0,0 +1,52 @@
+class city{
+    #name;
+    #country;
+    #description;
+    #humidity;
+    #wind;
+
+    get name(){
+
+        return this.#name;
+    }
+
+    set name(value){
+        this.#_name = value;
+    }
+    get country(){
+
+        return this.#country;
+    }
+
+    set country(value){
+        this.#country = value;
+    }
+
+    get description(){
+
+        return this.#description;
+    }
+
+    set description(value){
+        this.#description = value;
+    }
+
+    get humidity(){
+
+        return this.#humidity;
+    }
+
+    set humidity(value){
+        this.#humidity = value;
+    }
+
+    get wind(){
+
+        return this.#wind;
+    }
+
+    set wind(value){
+        this.#wind = value;
+    }
+
+} 
--- code/script/weather.js
+++ code/script/weather.js
@@ -6,27 +6,29 @@ let city;
 let form;
 
 // treatment 
-document.getElementsByClassName('searchForm').addEventListener('submit', function show(event){
+document.getElementById('searchForm').addEventListener("submit", function show(event){
     event.preventDefault();
     // input 
     search = document.getElementById('searchBar').value;
     url = "https://api.openweathermap.org/data/2.5/weather?q="+search+"&appid=0bf1faee1a5ca7e5e899dd845e4e0d44";
 
-
     fetch(url)
 
     .then(function(response){
+
         return response.json();
     })
 
-    // output
     .then(function(data){
-        
-        console.log(data.main.temp);
+
+        function insert(){
+            
+        }
     })
 
     .catch(function(err){
 
-        alert('You entered a wrong city name');
+        alert('wrong city');
     })
+    
 });
