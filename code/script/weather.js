5f348ab40bfa57b44a7c320e52790e31fd12e221
reda hammada <hammada.reda.dev@gmail.com>
2022-02-09 16:51:03 +0100

    update

--- code/script/weather.js
+++ code/script/weather.js
@@ -13,6 +13,7 @@ document.getElementById('searchForm').value ="";
 // event 
 document.getElementById('searchForm').addEventListener("submit", function show(event){
     event.preventDefault();
+  
     document.getElementById('info-container').style.display = "flex";
     search = document.getElementById('searchBar').value;
     url = "https://api.openweathermap.org/data/2.5/weather?q="+search+"&appid=0bf1faee1a5ca7e5e899dd845e4e0d44&units=metric";
@@ -32,13 +33,15 @@ document.getElementById('searchForm').addEventListener("submit", function show(e
          city.country = data.sys.country;
          city.description = data.weather[0].description;
          city.humidity = data.main.temp;
-         
-
          document.getElementById('info1').innerHTML = city.name + ',' + city.country ;
          document.getElementById('info2').innerHTML = city.description;
          document.getElementById('info3').innerHTML = city.humidity;
 
     })
+
+     .catch(function err(){
+        alert("You have entered a wrong city name ");
+    });
 
           
 });
